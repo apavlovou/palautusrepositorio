6ecbbf81b9a7450604f239b4172447e9a1f46dce
Alexey Pavlov <alexey.a.pavlov@utu.fi>
2025-11-23 14:55:45 +0200

    Teht 3

---
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -0,0 +1,90 @@
+import unittest
+from unittest.mock import Mock, ANY
+from kauppa import Kauppa
+from viitegeneraattori import Viitegeneraattori
+from varasto import Varasto
+from tuote import Tuote
+
+class TestKauppa(unittest.TestCase):
+    def setUp(self):
+        self.pankki_mock = Mock()
+        self.viitegeneraattori_mock = Mock()
+        
+        # palautetaan aina arvo 42
+        self.viitegeneraattori_mock.uusi.return_value = 42
+
+        self.varasto_mock = Mock()
+
+        # tehdään toteutus saldo-metodille
+        def varasto_saldo(tuote_id):
+            if tuote_id == 1:
+                return 10
+            elif tuote_id == 2:
+                return 10
+            elif tuote_id == 3:
+                return 0
+
+        # tehdään toteutus hae_tuote-metodille
+        def varasto_hae_tuote(tuote_id):
+            if tuote_id == 1:
+                return Tuote(1, "maito", 5)
+            elif tuote_id == 2:
+                return Tuote(2, "leipä", 3)
+            elif tuote_id == 3:
+                return Tuote(3, "voi", 8)
+
+        # otetaan toteutukset käyttöön
+        self.varasto_mock.saldo.side_effect = varasto_saldo
+        self.varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
+
+        # alustetaan kauppa
+        self.kauppa = Kauppa(self.varasto_mock, self.pankki_mock, self.viitegeneraattori_mock)
+
+    def test_maksettaessa_ostos_pankin_metodia_tilisiirto_kutsutaan(self):
+        # tehdään ostokset
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        # varmistetaan, että metodia tilisiirto on kutsuttu
+        self.pankki_mock.tilisiirto.assert_called()
+
+    def test_tilisiirto_kutsutaan_oikeilla_parametreilla_kun_ostetaan_yksi_tuote(self):
+        # tehdään ostokset
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        # varmistetaan, että metodia tilisiirto on kutsuttu oikeilla parametreilla
+        # tilisiirto(nimi, viite, tilinumero, kaupan_tili, summa)
+        self.pankki_mock.tilisiirto.assert_called_with("pekka", 42, "12345", "33333-44455", 5)
+
+    def test_tilisiirto_kutsutaan_oikeilla_parametreilla_kun_ostetaan_kaksi_eri_tuotetta(self):
+        # tehdään ostokset
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)  # maito, hinta 5
+        self.kauppa.lisaa_koriin(2)  # leipä, hinta 3
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        # varmistetaan, että summa on 5 + 3 = 8
+        self.pankki_mock.tilisiirto.assert_called_with("pekka", 42, "12345", "33333-44455", 8)
+
+    def test_tilisiirto_kutsutaan_oikeilla_parametreilla_kun_ostetaan_kaksi_samaa_tuotetta(self):
+        # tehdään ostokset
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)  # maito, hinta 5
+        self.kauppa.lisaa_koriin(1)  # maito, hinta 5
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        # varmistetaan, että summa on 5 + 5 = 10
+        self.pankki_mock.tilisiirto.assert_called_with("pekka", 42, "12345", "33333-44455", 10)
+
+    def test_tilisiirto_kutsutaan_oikeilla_parametreilla_kun_yksi_tuote_on_loppu(self):
+        # tehdään ostokset
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)  # maito, hinta 5, varastossa
+        self.kauppa.lisaa_koriin(3)  # voi, hinta 8, loppu (saldo 0)
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        # varmistetaan, että summa on vain 5, koska voi ei ole varastossa
+        self.pankki_mock.tilisiirto.assert_called_with("pekka", 42, "12345", "33333-44455", 5)
